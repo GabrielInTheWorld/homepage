2334cf391e63a8be5c88376f56bd52dba5efb69a
GabrielInTheWorld <meyergabriel@live.de>
2018-01-25 14:35:20 +0100

    Created footer
    ** added link to icons8 for every website

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Footer extends Component{
+    render(){
+        return(
+            <div id="footer">
+                <a href="https://icons8.com/" target="_blank" rel="noopener noreferrer">Link zu icons8</a>
+            </div>
+        )
+    }
+}
+
+export default Footer
